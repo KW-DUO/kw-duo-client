022d8c581f419136edeef73a14d6dd8f5e5673c9
Octoping <myc1365@naver.com>
2024-04-11 00:54:03 +0900

    feat: Post 타입 추가 및 더미데이터 추가

---
+++ dummy/post.ts
@@ -0,0 +1,15 @@
+import { PostCard } from '@/types/post';
+
+export const DUMMY_PROJECTS: PostCard[] = [
+  {
+    id: 1,
+    type: '🏫 수업 프로젝트',
+    date: new Date('2024-01-01'),
+    title: '[딥러닝 실습 팀원 구합니다!]',
+    department: '소프트웨어학부',
+    course: '딥러닝 실습',
+    position: ['상관 없음'],
+    techStack: ['spring'],
+    nickname: '닉네임',
+  },
+];
