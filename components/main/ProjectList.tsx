cbf75ef1c679bb071611b5bfa8e990a8d4e1d3a7
Octoping <myc1365@naver.com>
2024-04-11 01:04:52 +0900

    refactor: ProjectList의 key에 idx 제거

--- components/main/ProjectList.tsx
+++ components/main/ProjectList.tsx
@@ -8,9 +8,8 @@ type ProjectListProps = {
 const ProjectList = ({ posts }: ProjectListProps) => {
   return (
     <ul className="max-w-maxWidth grid grid-cols-4 gap-7">
-      {posts.map((post, idx) => (
-        // 실제 데이터에선 id가 제대로 올테니 key값을 id로 설정해주면 됨
-        <ProjectCard key={idx} project={post} />
+      {posts.map((post) => (
+        <ProjectCard key={post.id} project={post} />
       ))}
     </ul>
   );
