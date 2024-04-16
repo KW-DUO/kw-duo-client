f7073a064ca55786f485f4fe70c7bd0338c39306
Daewony <alexnori.com@gmail.com>
2024-04-16 11:00:04 +0900

    fix: tailwind.config plugin linecamp 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundColor: {
-        gray: "#F5F5F5",
-        lightGray: "#F9F9F9",
+        gray: '#F5F5F5',
+        lightGray: '#F9F9F9',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: "#A32828",
-        secondary: "#CC2E34",
-        silver: "#646464",
-        "dark-gray": "#3E4042",
-        
+        primary: '#A32828',
+        secondary: '#CC2E34',
+        silver: '#646464',
+        'dark-gray': '#3E4042',
       },
 
       maxWidth: {
-        maxWidth: "1300px",
+        maxWidth: '1300px',
       },
       width: {
-        cardWidth: "300px",
+        cardWidth: '300px',
       },
       height: {
-        cardHeight: "400px",
+        cardHeight: '400px',
       },
       borderColor: {
-        grayBorder: "#D1D1D1",
+        grayBorder: '#D1D1D1',
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [],
 };
 export default config;
