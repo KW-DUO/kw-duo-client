b5c0ee9415f0967e12e208426ddb084ff8955a74
Daewony <alexnori.com@gmail.com>
2024-04-21 10:35:17 +0900

    feat: constant 기술스택 정보 추가

---
+++ constant/techStack/index.ts
@@ -0,0 +1,39 @@
+export const techStack = [
+  // 프론트엔드 기술 스택
+  { label: 'React', value: 'react' },
+  { label: 'Next.js', value: 'next_js' },
+  { label: 'JavaScript', value: 'javascript' },
+  { label: 'TypeScript', value: 'typescript' },
+
+  // 백엔드 기술 스택
+  { label: 'Spring', value: 'spring' },
+  { label: 'Node.js', value: 'node_js' },
+  { label: 'Flask', value: 'flask' },
+  { label: 'NestJS', value: 'nestjs' },
+  { label: 'Django', value: 'django' },
+
+  // 데이터베이스 기술 스택
+  { label: 'MySQL', value: 'mysql' },
+  { label: 'MongoDB', value: 'mongodb' },
+  { label: 'Redis', value: 'redis' },
+
+  // 모바일 개발 기술 스택
+  { label: 'Kotlin', value: 'kotlin' },
+  { label: 'Swift', value: 'swift' },
+  { label: 'Flutter', value: 'flutter' },
+
+  // 머신러닝 기술 스택
+  { label: 'Python', value: 'python' }, // 오타 수정: pytyon -> Python
+  { label: 'TensorFlow', value: 'tensorflow' },
+  { label: 'PyTorch', value: 'pytorch' },
+
+  // 게임 개발 기술 스택
+  { label: 'Unity', value: 'unity' },
+  { label: 'Unreal Engine', value: 'unreal_engine' },
+
+  // 기타 기술 스택
+  { label: 'C', value: 'c' },
+  { label: 'C++', value: 'cpp' },
+  { label: 'C#', value: 'csharp' },
+  { label: 'Java', value: 'java' },
+] as const;
