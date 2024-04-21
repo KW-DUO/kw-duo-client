709ba0eb024aacdc22a6aa6b570f2df5f001b8b0
Daewony <alexnori.com@gmail.com>
2024-04-21 10:36:07 +0900

    feat: constant 포지션 정보 추가

--- constant/position/index.ts
+++ constant/position/index.ts
@@ -1,7 +1,14 @@
 export const positions = [
+  { label: '상관없음', value: 'ANY' },
   { label: '프론트엔드', value: 'FRONTEND' },
   { label: '백엔드', value: 'BACKEND' },
   { label: '안드로이드', value: 'ANDROID' },
   { label: 'iOS', value: 'IOS' },
   { label: '게임', value: 'GAME' },
+  { label: '기획자', value: 'GAME' },
+  { label: '디자이너', value: 'GAME' },
+  { label: '머신러닝', value: 'GAME' },
+  { label: '블록체인', value: 'GAME' },
+  { label: '임베디드', value: 'GAME' },
+  { label: '기타', value: 'OTHER' },
 ] as const;
