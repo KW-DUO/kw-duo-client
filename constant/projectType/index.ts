f27fada0054a6305681128b4f990ea559398a9bc
Daewony <alexnori.com@gmail.com>
2024-04-21 10:35:38 +0900

    feat: constant 프로젝트 종류 정보 추가

---
+++ constant/projectType/index.ts
@@ -0,0 +1,5 @@
+export const projectType = [
+  { label: '수업 프로젝트', value: 'CLASS_PROJECT' },
+  { label: '졸업 프로젝트', value: 'GRADUATION_PROJECT' },
+  { label: '사이드 프로젝트', value: 'SIDE_PROJECT' },
+];
