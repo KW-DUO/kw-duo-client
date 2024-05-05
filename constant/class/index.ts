a114db1c871726486cf53ea91396692ff52e0d77
Daewony <alexnori.com@gmail.com>
2024-05-05 17:45:14 +0900

    feat: constant 수정

--- constant/class/index.ts
+++ constant/class/index.ts
@@ -5,25 +5,25 @@ type classNameProps = {
 export const departmentClasses = ({ departmnet }: classNameProps) => {
   switch (departmnet) {
     case 'COMPUTER_INFORMATION_ENGINEERING':
-      return [{ value: 'algo', label: '소프트웨어공학' }];
+      return [{ value: '소프트웨어공학', label: '소프트웨어공학' }];
     case 'SOFTWARE':
       return [
-        { value: 'engineering_design', label: '공학설계입문' },
-        { value: 'creative_design', label: '창의설계입문' },
-        { value: 'data_structures_practice', label: '자료구조실습' },
-        { value: 'applied_software', label: '응용소프트웨어 실습' },
-        { value: 'deep_learning', label: '딥러닝 실습' },
+        { value: '공학설계입문', label: '공학설계입문' },
+        { value: '창의설계입문', label: '창의설계입문' },
+        { value: '자료구조실습', label: '자료구조실습' },
+        { value: '응용소프트웨어 실습', label: '응용소프트웨어 실습' },
+        { value: '딥러닝 실습', label: '딥러닝 실습' },
       ];
     case 'INFORMATION_CONVERGENCE':
       return [
-        { value: 'big_data_programming', label: '빅데이터프로그래밍(임동혁)' },
-        { value: 'text_mining', label: '텍스트마이닝(조민수)' },
-        { value: 'iot_system_design', label: 'IoT시스템설계및실습(박재성)' },
-        { value: 'ux_ui_design', label: 'UX/UI디자인(김현경)' },
-        { value: 'machine_learning', label: '기계학습(이상민)' },
-        { value: 'data_visualization', label: '데이터시각화(조재희)' },
+        { value: '빅데이터프로그래밍(임동혁)', label: '빅데이터프로그래밍(임동혁)' },
+        { value: '텍스트마이닝(조민수)', label: '텍스트마이닝(조민수)' },
+        { value: 'IoT시스템설계및실습(박재성)', label: 'IoT시스템설계및실습(박재성)' },
+        { value: 'UX/UI디자인(김현경)', label: 'UX/UI디자인(김현경)' },
+        { value: '기계학습(이상민)', label: '기계학습(이상민)' },
+        { value: '데이터시각화(조재희)', label: '데이터시각화(조재희)' },
       ];
     default:
-      return [{ value: 'undefined', label: '기타 및 없음' }];
+      return [{ value: '기타 및 없음', label: '기타 및 없음' }];
   }
 };
---
+++ constant/position/index.ts
@@ -0,0 +1,17 @@
+export const positions = [
+  { label: '프론트엔드', value: 'FRONTEND' },
+  { label: '백엔드', value: 'BACKEND' },
+  { label: '안드로이드', value: 'ANDROID' },
+  { label: 'iOS', value: 'IOS' },
+  { label: '게임 개발자', value: 'GAME_DEV' },
+  { label: '기획자', value: 'PLANNER' },
+  { label: '디자이너', value: 'DESIGNER' },
+  { label: '머신러닝', value: 'MACHINE_LEARNING' },
+  { label: '블록체인', value: 'BLOCKCHAIN' },
+  { label: '임베디드', value: 'EMBEDDED' },
+] as const;
+
+export function getPositionLabel(value: string): string {
+  const position = positions.find((pos) => pos.value === value);
+  return position ? position.label : 'Unknown';
+}
