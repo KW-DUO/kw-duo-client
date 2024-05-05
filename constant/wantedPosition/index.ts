2a9290f566c6c46db71c3c2b79462d6d4b34a19f
Daewony <alexnori.com@gmail.com>
2024-05-05 17:52:22 +0900

    feat: wantedPosition value 값 수정

--- constant/wantedPosition/index.ts
+++ constant/wantedPosition/index.ts
@@ -4,12 +4,12 @@ export const wantedPosition = [
   { label: '백엔드', value: 'BACKEND' },
   { label: '안드로이드', value: 'ANDROID' },
   { label: 'iOS', value: 'IOS' },
-  { label: '게임', value: 'GAME' },
-  { label: '기획자', value: 'GAME' },
-  { label: '디자이너', value: 'GAME' },
-  { label: '머신러닝', value: 'GAME' },
-  { label: '블록체인', value: 'GAME' },
-  { label: '임베디드', value: 'GAME' },
+  { label: '게임 개발자', value: 'GAME_DEV' },
+  { label: '기획자', value: 'PLANNER' },
+  { label: '디자이너', value: 'DESIGNER' },
+  { label: '머신러닝', value: 'MACHINE_LEARNING' },
+  { label: '블록체인', value: 'BLOCKCHAIN' },
+  { label: '임베디드', value: 'EMBEDDED' },
   { label: '기타', value: 'OTHER' },
 ] as const;
 
