7176a7ea2ee0b44094aa01ba43be462db3093acc
Daewony <alexnori.com@gmail.com>
2024-05-05 17:51:08 +0900

    design: 간편 로그인 UI 구현

---
+++ components/Login/LoginModal/SocialLoginModal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import LoginModal from './LoginModal';
+import { KakaoLoginButton, NaverLoginButton } from '../LoginButton/LoginButton';
+
+const SocialLoginModal = () => {
+  return (
+    <LoginModal>
+      <div className="flex justify-center items-center h-[440px]">
+        <div>
+          <h1 className="text-center mb-14 text-3xl font-bold">간편 로그인 후 이용 가능합니다!</h1>
+          <NaverLoginButton />
+          <KakaoLoginButton />
+        </div>
+      </div>
+    </LoginModal>
+  );
+};
+
+export default SocialLoginModal;
