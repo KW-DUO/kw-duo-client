59640e9dc2e4e7c7c0668e570f7f5ba6f316011e
Octoping <myc1365@naver.com>
2024-05-07 01:11:47 +0900

    chore: store.ts에 todo 추가

--- components/postDetail/store.ts
+++ components/postDetail/store.ts
@@ -3,5 +3,5 @@ import { createContext } from 'react';
 export const PostDetailContext = createContext(null);
 
 export function usePostDetailContext() {
-  return PostDetailContext;
+  // TODO: 로직 구현
 }
