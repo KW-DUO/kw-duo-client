52af08e6533a74d8918c369d49fc62d5d1a12b4f
Octoping <myc1365@naver.com>
2024-05-08 00:21:47 +0900

    feat: 버튼들에 useContext와 이벤트 연동

--- components/postDetail/ApplicationButton.tsx
+++ components/postDetail/ApplicationButton.tsx
@@ -1,6 +1,24 @@
+'use client';
+import { useContext } from 'react';
+import { PostDetailContext } from '@/components/postDetail/store';
+
 export const ApplicationButton = () => {
+  const post = useContext(PostDetailContext);
+
+  async function onClickClose() {
+    if (!post) {
+      alert('게시글 정보를 불러오는 중입니다. 잠시만 기다려주세요.');
+      return;
+    }
+
+    alert(`${post.id}번 게시글 지원 api 발사`);
+  }
+
   return (
-    <button className="w-full h-[70px] bg-secondary rounded text-white font-bold text-2xl mb-14">
+    <button
+      onClick={onClickClose}
+      className="w-full h-[70px] bg-secondary rounded text-white font-bold text-2xl mb-14"
+    >
       지원하기
     </button>
   );
