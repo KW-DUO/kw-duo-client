a5d95b19f78b39b07beea979b73db82db52be99d
Daewony <alexnori.com@gmail.com>
2024-05-08 23:20:09 +0900

    feat: 프로젝트 타입 value->label find 함수 추가

--- constant/projectType/index.ts
+++ constant/projectType/index.ts
@@ -1,4 +1,5 @@
 export const projectType = [
+  { label: '전체', value: 'ALL' },
   { label: '수업 프로젝트', value: 'CLASS_PROJECT' },
   { label: '졸업 프로젝트', value: 'GRADUATION_PROJECT' },
   { label: '사이드 프로젝트', value: 'SIDE_PROJECT' },
@@ -8,4 +9,9 @@ export const projectType = [
 export function getProjectTypeLabel(projectTypeValue: string): string {
   const type = projectType.find((type) => type.value === projectTypeValue);
   return type ? type.label : projectTypeValue; // 일치하는 타입이 있으면 label 반환, 없으면 입력값 그대로 반환
+}
+
+export function getProjectTypeValue(projectTypeLabel: string): string {
+  const type = projectType.find((type) => type.label === projectTypeLabel);
+  return type ? type.value : projectTypeLabel;
 }
