d2bf1416b049c214b3977bfb2ce3b1477915fe6b
Daewony <alexnori.com@gmail.com>
2024-05-08 22:19:24 +0900

    feat: 로그인 멀티 모달창 step 컴포넌트 추가

--- components/navBar/Navbar.tsx
+++ components/navBar/Navbar.tsx
@@ -1,24 +1,39 @@
+'use client';
+
 import Link from 'next/link';
 import Links from './links/Links';
 import { UserIcon } from 'lucide-react';
+import { useState } from 'react';
+import LoginStep from '../Login/LoginStep/LoginStep';
 
 const Navbar = () => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false); // 모달 표시 상태를 관리합니다.
+
+  const handleLoginButtonClick = () => {
+    setIsLoginModalOpen(true); // 로그인 버튼 클릭 시 모달을 엽니다.
+  };
+
   return (
-    <nav className="bg-secondary h-20">
-      <div className="max-w-maxWidth mx-auto h-full flex text-white justify-between items-center text-xl">
-        <Link href="/" className="text-2xl font-semibold">
-          KW DUO
-        </Link>
-        <div className="flex items-center">
-          <Links />
-          <Link href="/mypage">
-            <button>
-              <UserIcon size={30} />
-            </button>
+    <>
+      <nav className="bg-secondary h-20">
+        <div className="max-w-maxWidth mx-auto h-full flex text-white justify-between items-center text-xl">
+          <Link href="/" className="text-2xl font-semibold">
+            KW DUO
           </Link>
+          <div className="flex items-center">
+            <Links />
+            <button onClick={handleLoginButtonClick}>로그인</button>
+            <Link href="/mypage">
+              <button>
+                <UserIcon size={30} />
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {isLoginModalOpen && <LoginStep />}
+    </>
   );
 };
 
