fee80f85575b15ea62f99daa380c25ff3e758917
Daewony <alexnori.com@gmail.com>
2024-05-20 12:23:25 +0900

    feat: 팀원 구하기, 팀 구하기 토글에 따른 placeholder 내용 변경

--- components/Editor/Tiptap.tsx
+++ components/Editor/Tiptap.tsx
@@ -5,7 +5,7 @@ import StarterKit from '@tiptap/starter-kit';
 import Toolbar from './Toolbar';
 import Underline from '@tiptap/extension-underline';
 import Placeholder from '@tiptap/extension-placeholder';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 type TiptapProps = {
   onChange: (newContent: string) => void;
@@ -18,12 +18,9 @@ const Tiptap = ({ onChange, toggleState, initialValue }: TiptapProps) => {
     onChange(newContent);
   };
 
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Underline,
-      Placeholder.configure({
-        placeholder: `
+  const getPlaceholderText = useCallback(() => {
+    return toggleState
+      ? `
           [개발 프로젝트 모집 내용 예시]
           프로젝트 주제:
           프로젝트 목표:
@@ -31,7 +28,24 @@ const Tiptap = ({ onChange, toggleState, initialValue }: TiptapProps) => {
           프로젝트 관련 주의사항:
           프로젝트 포지션별 모집인원:
           기술 스택:
-        `,
+        `
+      : `
+          [자기소개 작성 내용 예시]
+          자신의 장점 어필:
+          기술 스택 어필:
+          관련 경험:
+          프로젝트 참여 동기:
+          팀과의 협업 경험:
+        `;
+  }, [toggleState]);
+
+  // Tiptap 에디터 초기화
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Placeholder.configure({
+        placeholder: getPlaceholderText(),
       }),
     ],
     content: '',
@@ -46,17 +60,28 @@ const Tiptap = ({ onChange, toggleState, initialValue }: TiptapProps) => {
     },
   });
 
+  // 초기값 변경 시 에디터 내용 업데이트
   useEffect(() => {
     if (editor) {
-      editor.commands.setContent(initialValue); // Update content when initialValue changes
+      editor.commands.setContent(initialValue);
     }
   }, [initialValue, editor]);
 
+  // toggleState(팀원,팀 구하기 토글) 변경 시, 이벤트 처리
   useEffect(() => {
     if (editor) {
-      editor.commands.setContent(''); // toggleState가 변경될 때마다 에디터 내용을 초기화합니다.
+      editor.commands.setContent('');
+
+      // Placeholder 확장을 재설정
+      editor.extensionManager.extensions.forEach((extension) => {
+        if (extension.name === 'placeholder') {
+          extension.options.placeholder = getPlaceholderText();
+        }
+      });
+      // 에디터 상태를 업데이트하여 새로운 Placeholder 적용
+      editor.view.updateState(editor.state);
     }
-  }, [toggleState, editor]);
+  }, [toggleState, editor, getPlaceholderText]);
 
   return (
     <div className="w-full">
