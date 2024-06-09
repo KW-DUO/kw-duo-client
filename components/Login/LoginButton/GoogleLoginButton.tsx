bbd3bc519c214b9b6c63ba20a2f8e97291a55fc5
Daewony <alexnori.com@gmail.com>
2024-06-10 02:21:51 +0900

    fix: 구글 로고 이미지 깨짐 현상 임시로 img 태그 적용

--- components/Login/LoginButton/GoogleLoginButton.tsx
+++ components/Login/LoginButton/GoogleLoginButton.tsx
@@ -13,12 +13,18 @@ export const GoogleLoginButton = ({ onClick }: GoogleLoginButtonProps) => {
       className="bg-white w-[540px] h-[66px] mb-8 rounded-2xl text-black text-2xl flex justify-center items-center relative outline-none border-gray-400 border"
       onClick={onClick}
     >
-      <Image
+      {/* <Image
+      
         src={'/icons/loginIcons/google.svg'}
         width={40}
         height={40}
         alt="sns login for google"
         className="absolute left-10"
+      /> */}
+      <img
+        src="/icons/loginIcons/google.svg"
+        alt="sns login for google"
+        className="absolute left-10 w-10 h-10"
       />
       <p>{t('login.googleLogin')}</p>
     </button>
